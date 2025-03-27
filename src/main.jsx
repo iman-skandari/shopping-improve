f80212da8f276995fcda9177fd1ad7475d84e8c3
نام شما <imisk9919@gmail.com>
2025-03-27 10:14:48 +0330

    initialize main and styles

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage/index.jsx";
+import Shop from "./pages/Shop/index.jsx";
+import Layout from "./components/Layout/index.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
