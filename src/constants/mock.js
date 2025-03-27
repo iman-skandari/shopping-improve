039f56e7d80cb4353ab12b6ce49feb8cfe94b777
نام شما <imisk9919@gmail.com>
2025-03-27 10:16:17 +0330

    added mock for data in project

---
+++ src/constants/mock.js
@@ -0,0 +1,30 @@
+export const categories = [
+  {
+    id: 0,
+    image:
+      "https://cdn.dummyjson.com/products/images/smartphones/iPhone%2013%20Pro/2.png",
+    title: "Smart phones",
+    category: "smartphones",
+  },
+  {
+    id: 1,
+    image:
+      "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Airpods/2.png",
+    title: "Accessories",
+    category: "mobile-accessories",
+  },
+  {
+    id: 2,
+    image:
+      "https://cdn.dummyjson.com/products/images/tablets/Samsung%20Galaxy%20Tab%20S8%20Plus%20Grey/1.png",
+    title: "Tablets",
+    category: "tablets",
+  },
+  {
+    id: 3,
+    image:
+      "https://cdn.dummyjson.com/products/images/laptops/Asus%20Zenbook%20Pro%20Dual%20Screen%20Laptop/3.png",
+    title: "Laptops",
+    category: "Laptops",
+  },
+];
