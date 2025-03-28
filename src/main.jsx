3e27ef90de82882db14b8b0e7f60d10173db08fc
نام شما <imisk9919@gmail.com>
2025-03-28 09:34:55 +0330

    added routes and services for axios , styles for design

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage/index.jsx";
-import Shop from "./pages/Shop/index.jsx";
-import Layout from "./components/Layout/index.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <HomePage />,
-      },
-      {
-        path: "shop",
-        element: <Shop />,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/index";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
